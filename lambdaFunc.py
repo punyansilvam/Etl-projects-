92f44bb30eb7dbb47a6ee8f3ac18c7bcfd8db29d
punyansilvam <136739732+punyansilvam@users.noreply.github.com>
2023-10-31 21:54:57 +0530

    Add files via upload

---
+++ lambdaFunc.py
@@ -0,0 +1,10 @@
+multi = lambda x:x*2
+print(multi(10))
+add = lambda x,y:x+y
+print(add(2,55))
+def add(a,b):
+    return a+b
+print(add(5,6))
+oddeven = lambda num: True if num%2 == 0 else False
+print(oddeven(24))
+
